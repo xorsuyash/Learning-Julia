cac3b1c0ddd4024bfb6578429982718d86579663
xorsuyash <suyashsrivastavam87@gmail.com>
2023-01-03 18:19:43 +0530

    Number Type

---
+++ NumberType.jl
@@ -0,0 +1,19 @@
+struct MyRational <: Real 
+    num
+    den 
+end 
+MyRational(2,3)
+function ⨸(num,den)
+    MyRational(num,den)
+end 
+2⨸3
+import Base.+
+function +(r1::MyRational,r2::MyRational)
+    (r1.num*r2.den+r2.num*r1.den)⨸(r1.den*r2.den)
+end 
+2⨸3+5⨸4
+import Base.show 
+function show(io::IO,r::MyRational)
+    print(io,"$(r.num)⨸$(r.den)")
+end 
+2⨸3+5⨸4
